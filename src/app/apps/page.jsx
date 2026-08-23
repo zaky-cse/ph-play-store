a71709837622f25d234d7c521f9207cfe93ad1af
Zaky Hossain <zaky.cse@gmail.com>
2026-08-23 15:31:42 +0600

    Naming correction for components

--- src/app/apps/page.jsx
+++ src/app/apps/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const page = () => {
+const ApplicationPage = () => {
     return (
         <div>
             Application Page
@@ -8,4 +8,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default ApplicationPage;
--- src/app/installation/something/page.jsx
+++ src/app/installation/something/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const page = () => {
+const SomethingPage = () => {
     return (
         <div>
             Something Page
@@ -8,4 +8,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default SomethingPage;
